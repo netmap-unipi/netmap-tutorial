c9d8ca52de6efef87bb109a6c7b6d572636f6ae6
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2017-08-25 15:09:07 -0700

    flowgraph

---
+++ flowgraph-down.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -x
+
+sudo kill $(cat pids)
---
+++ flowgraph-up.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+
+PIDS=""
+
+sudo pkt-gen -i netmap:pipe{1 -f tx -s 10.0.0.1:7000-10.0.0.1:7010 -d 10.0.0.1:8000-10.0.0.1:8004 &> /dev/null &
+PIDS="$PIDS $!"
+sleep 0.5
+sudo ./fe -i netmap:pipe}1 -i netmap:pipe{2 -i netmap:pipe{3 -p 8000 -p 8001 &
+PIDS="$PIDS $!"
+sleep 0.5
+sudo ./swap -i netmap:pipe}2 -i netmap:pipe{4 &
+PIDS="$PIDS $!"
+sleep 0.5
+sudo ./swap -i netmap:pipe}3 -i netmap:pipe{5 &
+PIDS="$PIDS $!"
+sleep 0.5
+sudo ./sink -i netmap:pipe}4 -p 7000 &
+PIDS="$PIDS $!"
+sleep 0.5
+sudo ./sink -i netmap:pipe}5 -p 7001 &
+PIDS="$PIDS $!"
+sleep 0.5
+
+echo $PIDS > pids
